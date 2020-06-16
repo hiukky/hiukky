3f2b2098b67c088fb3469ea7edd09cf949ce6d40
Romullo <developermarsh@gmail.com>
2020-06-15 23:02:40 -0300

    chore(): view home improviments

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import { Col } from '@minily/components'
 
 import Layout from 'layout'
 
 import { Title } from 'components'
 
-import { Wrapper } from './styles'
+import { Wrapper, Brand, Child } from './styles'
 
 const Home: React.FC = () => (
   <Layout title="Home">
     <Wrapper>
-      <Title tag="h1" label="Romullo Sousa" />
-      <Title tag="p" label="Fullstack Developer" />
+      <Child size={{ xs: 12, lg: 6 }}>
+        <Title tag="h1" label="Romullo Sousa" />
+        <Title tag="p" label="Fullstack Developer" />
+      </Child>
+
+      <Child size={{ xs: 12, lg: 6 }}>
+        <Brand src={'assets/brand.png'} alt="Hiukky brand" />
+      </Child>
     </Wrapper>
   </Layout>
 )
--- src/pages/index/styles.ts
+++ src/pages/index/styles.ts
@@ -1,15 +1,19 @@
 import styled from 'styled-components'
-import { Row } from '@minily/components'
+import { Row, Col } from '@minily/components'
 import { Flex } from '@minily/tools'
 
 const Wrapper = styled(Row)`
   height: 100%;
-  ${Flex.container('center', 'center', { direction: 'column' })}
+  ${Flex.container('center', 'center', { wrap: 'wrap' })}
 `
 
-const Box = styled.div`
-  height: 100%;
-  width: 100%;
+const Child = styled(Col)`
+  ${Flex.container('flex-start', 'center', { direction: 'column' })}
 `
 
-export { Wrapper, Box }
+const Brand = styled.img`
+  max-width: 25rem;
+  max-height: 25rem;
+`
+
+export { Wrapper, Child, Brand }
