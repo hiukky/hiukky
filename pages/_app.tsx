960ac4eccbd9014319a40c09aa2a3db53520ff34
Romullo <developermarsh@gmail.com>
2020-07-03 16:47:15 -0300

    feat(): fullscreen mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,33 @@
 import React from 'react'
 import NextApp from 'next/app'
+import FullScreen from 'react-full-screen'
 
 import 'remixicon/fonts/remixicon.css'
 
 import { Normalize, GlobalStyle, ThemeProvider } from 'theme'
 
 export default class App extends NextApp {
-  componentDitMount() {
-    document.documentElement.requestFullscreen({ navigationUI: 'hide' })
+  state = {
+    fullScreen: false,
+  }
+
+  componentDidUpdate() {
+    if (window.innerWidth <= 576 && !this.state.fullScreen) {
+      this.setState({ fullScreen: true })
+    }
   }
 
   render() {
     const { Component, pageProps } = this.props
 
     return (
-      <>
+      <FullScreen enabled={this.state.fullScreen}>
         <Normalize />
         <GlobalStyle />
         <ThemeProvider>
           <Component {...pageProps} />
         </ThemeProvider>
-      </>
+      </FullScreen>
     )
   }
 }
