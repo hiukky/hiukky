5a531fdc138a226273b6bba172c3a720f5ceff8e
Romullo <developermarsh@gmail.com>
2020-10-12 12:22:44 -0300

    chore(pages): remove react full screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,22 @@
 import React from 'react'
 import NextApp from 'next/app'
-import FullScreen from 'react-full-screen'
 
 import 'remixicon/fonts/remixicon.css'
 
 import { Normalize, GlobalStyle, ThemeProvider } from 'theme'
 
 export default class App extends NextApp {
-  state = {
-    fullScreen: false,
-  }
-
-  componentDidUpdate() {
-    if (window.innerWidth <= 1140 && !this.state.fullScreen) {
-      this.setState({ fullScreen: true })
-    }
-  }
-
   render() {
     const { Component, pageProps } = this.props
 
     return (
-      <FullScreen enabled={this.state.fullScreen}>
+      <>
         <Normalize />
         <GlobalStyle />
         <ThemeProvider>
           <Component {...pageProps} />
         </ThemeProvider>
-      </FullScreen>
+      </>
     )
   }
 }
